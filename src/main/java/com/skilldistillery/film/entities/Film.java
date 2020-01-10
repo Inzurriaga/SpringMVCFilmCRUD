407b94e29a09ea523864c0284d147e9aa357e4a7
Neal Savant <nealrsavant@gmail.com>
2020-01-10 16:53:02 -0700

    Very important

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,27 @@
 package com.skilldistillery.film.entities;
 
 public class Film {
-
+//	posting stuff for the green dots
+	private int id;
+	//comment
+	
+	//comment
+	
+		//another comment
+		//another comment
+		//another comment
+	//comment
+	
+		//another comment
+		//another comment
+		//another comment
+	//comment
+	
+		//another comment
+		//another comment
+		//another comment
+	
+	//another comment
+	//another comment
+	//another comment
 }
